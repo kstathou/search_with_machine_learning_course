6a388ef71f2a5e1436a1ba6bdba867b360f46c8f
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2023-03-25 20:45:45 +0000

    wip

--- week4/utilities/index_products.py
+++ week4/utilities/index_products.py
@@ -20,10 +20,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
-# IMPLEMENT ME: import the sentence transformers module!
+from sentence_transformers import SentenceTransformer
 
 logger.info("Creating Model")
-# IMPLEMENT ME: instantiate the sentence transformer model!
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 # NOTE: this is not a complete list of fields.  If you wish to add more, put in the appropriate XPath expression.
 #TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
@@ -137,9 +137,14 @@ def index_file(file, index_name, reduced=False):
         if reduced and ('categoryPath' not in doc or 'Best Buy' not in doc['categoryPath'] or 'Movies & Music' in doc['categoryPath']):
             continue
         docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc})
+        names.append(doc['name'][0])
         #docs.append({'_index': index_name, '_source': doc})
         docs_indexed += 1
         if docs_indexed % 200 == 0:
+            embeddings = model.encode(names)
+            logger.info("Adding embeddings")
+            for doc, embedding in zip(docs, embeddings):
+                doc['_source']['embedding'] = embedding.tolist()
             logger.info("Indexing")
             bulk(client, docs, request_timeout=60)
             logger.info(f'{docs_indexed} documents indexed')
